77bebd49545b41565fd12252c2ce047bbdd0fcc6
AndreyKholopov <Andrey.Kholopov.Work@gmail.com>
2020-01-11 21:08:51 +0300

    add responsive for header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+// Select DOM Items
+const menuBtn = document.querySelector('.btn__menu');
+const menuNav = document.querySelector('.menu-list');
+
+//Set Initial State of Menu
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+	if(!showMenu) {
+		menuBtn.classList.add('close');
+		menuNav.classList.add('show');
+
+		showMenu = true;
+	} else {
+		menuBtn.classList.remove('close');
+		menuNav.classList.remove('show');
+
+		showMenu = false;
+	}
+}
+
 //form
 function show(state){
 	document.getElementById('gray').style.display = state;
