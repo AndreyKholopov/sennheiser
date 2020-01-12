265b52c3ee38cc7cb843f734d3809b22f48ad38a
AndreyKholopov <Andrey.Kholopov.Work@gmail.com>
2020-01-12 23:00:14 +0300

    add animated navbar

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 // Select DOM Items
 const menuBtn = document.querySelector('.btn__menu');
 const menuNav = document.querySelector('.menu-list');
+const sections = document.querySelectorAll('section');
+const bubble = document.querySelector('.bubble');
 
 //Set Initial State of Menu
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
 
+//responsive button
 function toggleMenu() {
 	if(!showMenu) {
 		menuBtn.classList.add('close');
@@ -20,6 +23,37 @@ function toggleMenu() {
 		showMenu = false;
 	}
 }
+
+//animated navbar
+const options = {
+	threshold: 0.7
+};
+
+let observer = new IntersectionObserver(navCheck, options);
+
+function navCheck(entries){
+	entries.forEach(entry => {
+		const className = entry.target.className;
+		const activeAnchor = document.querySelector(`[data-page=${className}]`);
+		const coords = activeAnchor.getBoundingClientRect();
+		const direction = {
+			height: coords.height,
+			width: coords.width,
+			top: coords.top,
+			left: coords.left
+		};
+		if (entry.isIntersecting) {
+			bubble.style.setProperty('left', `${direction.left}px`);
+			bubble.style.setProperty('top', `${direction.top}px`);
+			bubble.style.setProperty('width', `${direction.width}px`);
+			bubble.style.setProperty('height', `${direction.height}px`);
+		}
+	});
+}
+
+sections.forEach(section => {
+	observer.observe(section);
+});
 
 //form
 function show(state){
